b3da4eb4f78d69f964951da596a1f9c2ac9edd33
Jayaraj Sivadasan <jayarajsivadasan@pop-os.localdomain>
2021-04-21 20:45:07 -0400

    Python Packaging Example

---
+++ jaypdf/app.py
@@ -0,0 +1,6 @@
+from jaypdf2021 import pdf2text
+
+
+convert()
+
+# faHGq6LrBBHxwbv
---
+++ jaypdf/jaypdf2021/pdf2text.py
@@ -0,0 +1,15 @@
+""" Just to convert pdf to text 
+
+    Detailed explanation can be found online
+"""
+
+
+def convert():
+    """ 
+    Just call this method
+
+    Parameters: None
+
+    Returns: None
+    """
+    print("pdf2text")
